27808df428ae5617a7a2d223572264a21ef5ce16
NadzmiAR <rafinadzmi@gmail.com>
2022-11-16 13:46:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const words = ["adalah sebuah kota yang terletak di provinsi Jawa Barat, Bogor dikenal dengan julukan Kota Hujan,karena memiliki curah hujan yang lumayan sangat tinggi"]
+
+
+let cursor = gsap.to('.cursor', {opacity: 0, ease:"power2.inOut", repeat: -1})
+
+let masterTl = gsap.timeline({repeat: -1})
+
+words.forEach(word => {
+    let tl = gsap.timeline({repeat: 1.5, yoyo: true, repeatDelay: 2})
+    tl.to('.text', {duration: 10, text: word})
+    masterTl.add(tl)
+})
